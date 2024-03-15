c6b2d26d767fbd7c0a9155341b3e5a9931021e0a
ramo-dev <annuar.dev@gmail.com>
2024-03-15 15:38:53 +0300

    athenians

--- server/athenians.js
+++ server/athenians.js
@@ -3,50 +3,136 @@ const http = require("http");
 const app = express();
 const bodyParser = require("body-parser");
 const server = http.createServer(app);
+const location = [
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2024",
+  },
+  {
+    events: "Nyama Fests",
+    location: "Juja",
+    date: "1-2-2024",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Kroad",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2024",
+  },
+  {
+    events: "Nyama Fests",
+    location: "Juja",
+    date: "1-2-2024",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Drinks and Link",
+    location: "Kroad",
+    date: "12-3-2022",
+  },
+  {
+    events: "Nyama choma",
+    location: "Thika",
+    date: "12-3-2022",
+  },
+  {
+    events: "Pool Party",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+];
 
 // const api = "52239c6ce3fdd8f46102821a5476e3ad9d752e6fbd09eec32eb5c40775221801";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let locs = [];
+
+location.forEach((loc) => {
+  locs.push(loc.location.toLocaleLowerCase());
+  return locs;
+});
+
+function returnEvents(locationName) {
+  const eventsInLocation = location.filter(
+    (loc) => loc.location.toLowerCase() === locationName.toLowerCase()
+  );
+  return eventsInLocation;
+}
+
+const events = location.map((event) => {
+  return event.event;
+});
+
+const eventsCount = events.length;
+let eventsList = "";
+
+// Iterate over the events array to create a list
+events.forEach((event, index) => {
+  // eventsList += `${index + 1}. ${event.name}: ${event.details}\n`;
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post("/ussd", (req, res) => {
-  // Read the variables sent via POST from our API
-  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+  const { text } = req.body;
 
   let response = "";
 
-  if (text == "") {
-    // This is the first request. Note how we start the response with CON
-    response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
-  } else if (text == "1") {
-    // Business logic for first level response
-    response = `CON Choose account information you want to view
-        1. Account number`;
-  } else if (text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    response = `END Your phone number is ${phoneNumber}`;
-  } else if (text == "1*1") {
-    // This is a second level response where the user selected 1 in the first instance
-    const accountNumber = "ACC100101";
-    // This is a terminal request. Note how we start the response with END
-    response = `END Your account number is ${accountNumber}`;
+  if (text === "") {
+    response = `CON Welcome to Mafichoni Baze.
+        Where ..............
+        Where are You?
+        1. Juja
+        2. Kroad
+        3. Thika
+        4. Ruiru
+        `;
+  } else if (text === "1" || text === "2" || text === "3" || text === "4") {
+    const locationIndex = parseInt(text) - 1;
+    const locationNames = ["Juja", "Kroad", "Thika", "Ruiru"];
+    const selectedLocation = locationNames[locationIndex];
+    const events = returnEvents(selectedLocation);
+    if (events.length > 0) {
+      response = `CON Events in ${selectedLocation}:\n`;
+      events.forEach((event) => {
+        response += `${event.events} - ${event.date}\n`;
+      });
+    }
   }
 
-  // Send the response back to the API
-  res.set("Content-Type: text/plain");
+  res.set("Content-Type", "text/plain");
   res.send(response);
 });
 
+server.listen(3001, () => {
+  console.log("Its alive");
+});
 
 
-
-
-
-
-
-server.listen(3001, ()=>{
-    console.log('Its alive')
-})
---
+++ server/data.js
@@ -0,0 +1,62 @@
+export const location = [
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2024",
+  },
+  {
+    events: "Nyama Fests",
+    location: "Juja",
+    date: "1-2-2024",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Kroad",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Kuku fest",
+    location: "Thika",
+    date: "12-3-2024",
+  },
+  {
+    events: "Nyama Fests",
+    location: "Juja",
+    date: "1-2-2024",
+  },
+  {
+    events: "Kuku fest",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+  {
+    events: "Drinks and Link",
+    location: "Kroad",
+    date: "12-3-2022",
+  },
+  {
+    events: "Nyama choma",
+    location: "Thika",
+    date: "12-3-2022",
+  },
+  {
+    events: "Pool Party",
+    location: "Ruiru",
+    date: "12-3-2022",
+  },
+];
